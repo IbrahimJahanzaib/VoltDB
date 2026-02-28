955ac0d36515f547f26412a0ef24e1c63449a3f6
Ibrahim Arif <ibrahimjahanzaib2001@gmail.com>
2026-02-28 23:58:06 +0100

    Basic implementation of multiple PING commands

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import socket
 
 def main():
-
-    server_socket = socket.create_server("localhosy", 6379, reuse_port=True)
-    server_socket.accept()
-
-    connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    
+    while True:
+        connection, _ = server_socket.accept()
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            command = data.decode('utf-8')
+            print(f"Received command: {command}")
+            # Basic RESP parsing: check if it's a PING command
+            if "PING" in command.upper():
+                connection.sendall(b"+PONG\r\n")
+        connection.close()
 
 if __name__ == "__main__":
     main()
