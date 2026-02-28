f6a0d5a05a6e137d6e9b6dbddde8393e7ea98b39
Ibrahim Arif <ibrahimjahanzaib2001@gmail.com>
2026-02-28 23:58:06 +0100

    Implemented PING cmd

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ def main():
     server_socket = socket.create_server("localhosy", 6379, reuse_port=True)
     server_socket.accept()
 
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+PONG\r\n")
+
 if __name__ == "__main__":
     main()
